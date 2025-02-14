d26e2a9ca0ba726e1cb5017da2494473c68bc542
Favas Muhammed <favassakkeer13@gmail.com>
2025-02-14 16:47:54 +0100

    base 1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>The Brand Collector</h1>
+    </nav>
+  );
+}
